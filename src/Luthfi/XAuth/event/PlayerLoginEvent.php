4025e17f7c1910dacd6eebdf668c57d3cc5a1e91
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-07-31 14:23:36 +0300

    feat: Make PlayerLoginEvent cancellable

--- src/Luthfi/XAuth/event/PlayerLoginEvent.php
+++ src/Luthfi/XAuth/event/PlayerLoginEvent.php
@@ -6,8 +6,11 @@ namespace Luthfi\XAuth\event;
 
 use pocketmine\event\player\PlayerEvent;
 use pocketmine\player\Player;
+use pocketmine\event\Cancellable;
+use pocketmine\event\CancellableTrait;
 
-class PlayerLoginEvent extends PlayerEvent {
+class PlayerLoginEvent extends PlayerEvent implements Cancellable {
+    use CancellableTrait;
 
     public function __construct(Player $player) {
         $this->player = $player;
