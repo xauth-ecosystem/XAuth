bbe3c3eac32bae5374161b0fe0cab2c6b2d987cb
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-08-27 15:22:07 +0300

    refactor: Revert AbstractDataProvider initialization to simpler form

--- src/Luthfi/XAuth/database/AbstractDataProvider.php
+++ src/Luthfi/XAuth/database/AbstractDataProvider.php
@@ -58,9 +58,7 @@ abstract class AbstractDataProvider implements DataProviderInterface {
     public function initialize(): Await {
         return Await::f2c(function () use ($this) {
             try {
-                yield Await::f2c(function () use ($this) {
-                    return $this->connector->asyncGeneric('xauth.init');
-                });
+                yield $this->connector->asyncGeneric('xauth.init');
                 $this->plugin->getLogger()->debug("Database tables initialized.");
             } catch (SqlError $error) {
                 $this->plugin->getLogger()->error("Failed to initialize database tables: " . $error->getMessage());
