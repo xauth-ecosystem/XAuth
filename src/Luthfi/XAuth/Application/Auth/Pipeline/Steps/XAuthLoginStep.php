a430800c38c6ba32ef8c6b4daae607542a8f042c
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-06 13:26:40 +0300

    fix(auth-pipeline): Remove invalid parent::__construct() call in XAuthLoginStep

--- src/Luthfi/XAuth/Application/Auth/Pipeline/Steps/XAuthLoginStep.php
+++ src/Luthfi/XAuth/Application/Auth/Pipeline/Steps/XAuthLoginStep.php
@@ -55,7 +55,6 @@ class XAuthLoginStep implements AuthenticationStep, FinalizableStep {
         private ?UserRepository $userRepository,
         private KickTaskManager $kickTaskManager,
     ) {
-        parent::__construct();
     }
 
     public function getId(): string {
